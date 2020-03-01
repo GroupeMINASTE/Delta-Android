9e8c8c6e470a89db953412120e22783c72e4f825
Nathan Fallet <contact@nathanfallet.me>
2020-03-01 11:04:59 +0100

    I hate bugs in prod!

--- app/src/main/java/fr/zabricraft/delta/extensions/StringExtension.java
+++ app/src/main/java/fr/zabricraft/delta/extensions/StringExtension.java
@@ -107,40 +107,52 @@ public class StringExtension {
         SpannableStringBuilder workspace = new SpannableStringBuilder(string);
 
         // Powers (numbers)
-        Matcher numbers = Pattern.compile(" ?\\^ ?([0-9a-zA-Z]+)").matcher(workspace.toString());
-        while (numbers.find()) {
-            String group = numbers.group(1);
-            workspace.setSpan(new SuperscriptSpan(), numbers.start(), numbers.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.setSpan(new AbsoluteSizeSpan(10, true), numbers.start(), numbers.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.replace(numbers.start(), numbers.end(), group);
-        }
+        Matcher numbers = null;
+        do {
+            if (numbers != null) {
+                String group = numbers.group(1);
+                workspace.setSpan(new SuperscriptSpan(), numbers.start(), numbers.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.setSpan(new AbsoluteSizeSpan(10, true), numbers.start(), numbers.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.replace(numbers.start(), numbers.end(), group);
+            }
+            numbers = Pattern.compile(" ?\\^ ?([0-9a-zA-Z]+)").matcher(workspace.toString());
+        } while (numbers.find());
 
         // Powers (expressions)
-        Matcher expressions = Pattern.compile(" ?\\^ ?\\(([0-9a-zA-Z*+\\-/ ]+)\\)").matcher(workspace.toString());
-        while (expressions.find()) {
-            String group = expressions.group(1);
-            workspace.setSpan(new SuperscriptSpan(), expressions.start(), expressions.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.setSpan(new AbsoluteSizeSpan(10, true), expressions.start(), expressions.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.replace(expressions.start(), expressions.end(), group);
-        }
+        Matcher expressions = null;
+        do {
+            if (expressions != null) {
+                String group = expressions.group(1);
+                workspace.setSpan(new SuperscriptSpan(), expressions.start(), expressions.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.setSpan(new AbsoluteSizeSpan(10, true), expressions.start(), expressions.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.replace(expressions.start(), expressions.end(), group);
+            }
+            expressions = Pattern.compile(" ?\\^ ?\\(([0-9a-zA-Z*+\\-/ ]+)\\)").matcher(workspace.toString());
+        } while (expressions.find());
 
         // Indexes (variables)
-        Matcher variables = Pattern.compile("_([0-9a-zA-Z])").matcher(workspace.toString());
-        while (variables.find()) {
-            String group = variables.group(1);
-            workspace.setSpan(new SubscriptSpan(), variables.start(), variables.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.setSpan(new AbsoluteSizeSpan(10, true), variables.start(), variables.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.replace(variables.start(), variables.end(), group);
-        }
+        Matcher variables = null;
+        do {
+            if (variables != null) {
+                String group = variables.group(1);
+                workspace.setSpan(new SubscriptSpan(), variables.start(), variables.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.setSpan(new AbsoluteSizeSpan(10, true), variables.start(), variables.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.replace(variables.start(), variables.end(), group);
+            }
+            variables = Pattern.compile("_([0-9a-zA-Z])").matcher(workspace.toString());
+        } while (variables.find());
 
         // Indexes (expressions)
-        Matcher expressions2 = Pattern.compile("_\\(([0-9a-zA-Z*+\\-/ ]+)\\)").matcher(workspace.toString());
-        while (expressions2.find()) {
-            String group = expressions2.group(1);
-            workspace.setSpan(new SubscriptSpan(), expressions2.start(), expressions2.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.setSpan(new AbsoluteSizeSpan(10, true), expressions2.start(), expressions2.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            workspace.replace(expressions2.start(), expressions2.end(), group);
-        }
+        Matcher expressions2 = null;
+        do {
+            if (expressions2 != null) {
+                String group = expressions2.group(1);
+                workspace.setSpan(new SubscriptSpan(), expressions2.start(), expressions2.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.setSpan(new AbsoluteSizeSpan(10, true), expressions2.start(), expressions2.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                workspace.replace(expressions2.start(), expressions2.end(), group);
+            }
+            expressions2 = Pattern.compile("_\\(([0-9a-zA-Z*+\\-/ ]+)\\)").matcher(workspace.toString());
+        } while (expressions2.find());
 
         return workspace;
     }
